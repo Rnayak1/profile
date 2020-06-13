b8d0b3d4831b395eec730d2930d9234ee71a6c51
Rohit Nayak <rohitn116@gmail.com>
2020-06-13 13:02:55 +0530

    initial test for portfolio

---
+++ data.js
@@ -0,0 +1,15 @@
+data = {
+    "profile": `<h1> I am Profile </h1>`,
+};
+
+const dat = () => {
+    console.log(data)
+}
+
+const gitData = async (userName) => {
+    const gitData = await axios.get('https://api.bloggify.net/gh-calendar/?username=rnayak1')
+    console.log(typeof (gitData.data));
+    document.getElementById('git').innerHTML += gitData.data;
+    document.getElementsByClassName('contrib-footer')[0].style.display = "none"
+}
+gitData('rnayak1');
