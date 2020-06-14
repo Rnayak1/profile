6dc2671b2988b4bbb6b8dd9d81ee144ed94372dd
Rohit Nayak <rohitn116@gmail.com>
2020-06-14 14:20:48 +0530

    added minimal js

--- data.js
+++ data.js
@@ -6,10 +6,17 @@ const dat = () => {
     console.log(data)
 }
 
-const gitData = async (userName) => {
-    const gitData = await axios.get('https://api.bloggify.net/gh-calendar/?username=rnayak1')
-    console.log(typeof (gitData.data));
-    document.getElementById('git').innerHTML += gitData.data;
-    document.getElementsByClassName('contrib-footer')[0].style.display = "none"
+const gitData = async(userName) => {
+        const gitData = await axios.get('https://api.bloggify.net/gh-calendar/?username=rnayak1')
+        console.log(typeof(gitData.data));
+        document.getElementById('git').innerHTML += gitData.data;
+        document.getElementsByClassName('contrib-footer')[0].style.display = "none"
+    }
+    //gitData('rnayak1');
+
+const changeTheme = () => {
+    if (document.body.classList.contains('c-dark-theme'))
+        document.body.classList.remove('c-dark-theme')
+    else
+        document.body.classList.add('c-dark-theme')
 }
-gitData('rnayak1');
